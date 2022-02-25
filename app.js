12a7bb4fd75c06ea9ae0608e98ef6fe182e43d24
Simone823 <simone.daglio823@outlook.it>
2022-02-25 17:14:59 +0100

    modifica newlisttodo, aggiunta listCompleted

--- app.js
+++ app.js
@@ -13,27 +13,38 @@ const app = new Vue({
 
         // Messaggio input utente
         message: '',
-
-        // Todo completato, all'inizio è falso
-        done: false,
     },
 
     methods: {
 
         // Funzione crea nuovo oggetto lista
         newListTodo: function(){
-            if (this.message != 0){
-                this.arrayList.push({testo: this.message, done: this.done});
+
+            // Se il messaggio inserito dall'utente non è una strigna vuota 
+            if (this.message != ""){
+            
+                // pusho message nell'array di oggeti arrayList
+                this.arrayList.push({testo: this.message, done: false});
             } else {
                 alert("Inserisci un testo");
             }
+
+            // Resetto l'input quando l'utente clicca il pulsante add
             this.message = "";
         },
 
         // Funzione che cambia il done al click di check
-        listCompleted: function(){
-            this.done = true;
-            console.log(this.done);
+        listCompleted: function(el){
+
+            // Se la proprietà done del mio array oggeti arrayList è uguale a false
+            if (el.done == false){
+                
+                // Allora la proprietà done dell'elemento sarà true
+                el.done = true;
+            } else {
+                el.done = false;
+            }
+            console.log(el.done);
         }
     }
 });
