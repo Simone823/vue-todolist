7bff41a52802d359052a3ad28b25ed9bcd99f4d2
Simone823 <simone.daglio823@outlook.it>
2022-02-25 16:33:25 +0100

    v-model input, v-for li

---
+++ app.js
@@ -0,0 +1,33 @@
+// Vue JS 
+const app = new Vue({
+    el: "#app",
+    data: {
+
+        // Array oggetti lista
+        arrayList: [
+            {
+                testo: "Make todolist design",
+                done: false,
+            },
+        ],
+
+        // Messaggio input utente
+        message: '',
+
+        // Todo completato, all'inizio è falso
+        done: false,
+    },
+
+    methods: {
+
+        // Funzione crea nuovo oggetto lista
+        newListTodo: function(){
+            if (this.message != 0){
+                this.arrayList.push({testo: this.message, done: this.done});
+            } else {
+                alert("Inserisci un testo");
+            }
+            this.message = "";
+        }
+    }
+});
